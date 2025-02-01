867ca9a6b1a78f6b251a2a9ecab1e9a610caa2af
Hamza Asif <hamzaahmad122333@gmail.com>
2025-02-01 15:13:10 +0500

    update

--- src/app/404.tsx
+++ src/app/404.tsx
@@ -2,7 +2,7 @@
 const Custom404 = () => (
   <div className="container mx-auto py-16 px-4 bg-black text-white text-center">
     <h1 className="text-5xl font-bold mb-8">404 - Page Not Found</h1>
-    <p>The page you&apos ;re looking for doesn&apos ;t exist or has been moved.</p>
+    <p>The page you&apos;re looking for doesn&apos;t exist or has been moved.</p>
     <link  href="/" className="bg-blue-500 py-2 px-6 mt-8 rounded-md hover:bg-blue-700">
       Go Home</link>
     
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,14 +1,9 @@
 'use client';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 
 function Hero() {
-  const router = useRouter();
 
-  const handleNavigation = () => {
-    router.push('/shoplist'); // Change to the desired route
-  };
 
   return (
     <div className="relative w-full bg-[#F3F3F3] overflow-hidden flex flex-col md:flex-row items-center justify-between">
